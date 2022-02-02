dcef7cfd4dee06bdae47e1d534466fc9f1a694d8
Raul Romero Cano <raulromerocano@Rauls-MacBook-Air.local>
2022-02-02 13:35:45 -0330

    Updated tables

---
+++ tortoise_bin_v2.sql
@@ -0,0 +1,28 @@
+/* tortoise_bin_v2.sql
+
+createdb tortoise_bin;
+
+psql -d tortoise_bin -f tortoise_bin.sql
+
+psql -d tortoise_bin
+
+*/
+
+CREATE TABLE bins (
+  bin_id serial PRIMARY KEY,
+  bin_path char(8) NOT NULL,
+  origin_ip cidr,
+  creation_time timestamp NOT NULL
+);
+
+CREATE TABLE requests (
+  request_id serial PRIMARY KEY,
+  request_type char(8),
+  request_origin_ip cidr,
+  time_received timestamp,
+  headers varchar,
+  request_payload jsonb
+);
+
+ALTER TABLE requests ADD COLUMN bin_id integer REFERENCES bins (bin_id);
+ALTER TABLE requests ALTER COLUMN bin_id SET NOT NULL;
